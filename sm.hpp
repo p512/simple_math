3e922e359909ef956ea8527f5aea21a30aa06feb
liquidblock <liquidblock@users.noreply.github.com>
2015-09-28 20:41:13 +0200

    Added cos, pow, log, exp

--- sm.hpp
+++ sm.hpp
@@ -50,23 +50,110 @@ namespace sm {
         return e <= 0 ? 1 : b * pow(b, e-1);
     }
 
-    double sin(double x) {
-        constexpr int terms = 20; 
-        facs<unsigned long, terms> faclist;
-        
-        double mult = 1;
+    template<typename N>
+    class kahansum {
+    	N C, S, Y, T;
+    public:
+		kahansum(N value) { S = value; C = 0; }
+    	kahansum() : kahansum(0) {}
+    	kahansum<N>& operator +=(N value)
+    	{
+    		Y = value - C;
+    		T = S + Y;
+    		C = (T - S) - Y;
+    		S = T;
+    		return *this;
+    	}
+    	kahansum<N>& operator -=(N value)
+    	{
+    		Y = -value - C;
+    		T = S + Y;
+    		C = (T - S) - Y;
+    		S = T;
+    		return *this;
+    	}
+    	operator N()
+    	{
+    		return S;
+    	}
+    };
+    
+    template<typename N, N (func)(double x, int i, unsigned long fac), int start, int increment, bool add_x>
+    static inline N sum(N x) {
+        constexpr int terms = 30; 
+        facs<double, terms> faclist;
+        kahansum<N> sum(add_x ? x : 0); 
+        for(int i = start; i < terms; i+=increment) {
+            sum += func(x, i, faclist[i]);
+        }
+        return (N)sum;
+    }
+    
+    template<typename N>
+    static inline N sin_term(N x, int i, unsigned long fac)
+	{
+		return (i/2 & 1 ? -1 : 1) * pow(x,i)/fac;
+	}
+	
+	template<typename N>
+	static inline N cos_term(N x, int i, unsigned long fac)
+	{
+		return (i/2 & 1 ? -1 : 1) * pow(x,i)/fac;
+	}
+    
+    template<typename N>
+    static inline N exp_term(N x, int i, unsigned long fac)
+	{
+		return pow(x,i)/fac;
+	}
+	
+	template<typename N>
+	static inline N log_term1(N x, int i, unsigned long fac)
+	{
+		return pow(-1, i + 1) * pow(x - 1, i)/ (N)i ;
+	}
+	
+	template<typename N>
+	static inline N log_term2(N x, int i, unsigned long fac)
+	{
+		return pow((x - 1) / x, i) / (N)i ;
+	}
+    
+    template<typename N>
+    N sin(N x)
+    {
+    	N mult = 1;
         if(x >= M_PI) {
             x -= M_PI;
             mult = -1;
-        } 
-        double sum = x; 
-        for(int i = 3; i < terms; i+=2) {
-            double term = pow(x,i)/faclist[i];
-            if(i/2 & 1)
-                sum -= term;
-            else
-                sum += term;
         }
-        return sum *= mult;
+    	return sum<N, sin_term<N>, 3, 2, true>(x) * mult;
+    }
+    
+    template<typename N>
+    N cos(N x)
+    {
+    	return sum<N, cos_term<N>, 0, 2, false>(x);
+    }
+    
+    template<typename N>
+    N exp(N x)
+    {
+    	return sum<N, exp_term<N>, 0, 1, false>(x);
+    }
+    
+    template<typename N>
+    N log(N x)
+    {
+    	if(x <= 0.5)
+    		return sum<N, log_term1<N>, 1, 1, false>(x);
+    	else
+    		return sum<N, log_term2<N>, 1, 1, false>(x);
+    }
+    
+    template<typename N>
+    N pow(N x, N y)
+    {
+    	return exp(y * log(x));
     }
 }

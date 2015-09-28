004fccd2b040532be31c246539832776932abfa8
p512 <point6000x+gh@gmail.com>
2015-09-29 00:11:09 +0200

    Added testcases with generous epsilon

--- test.cpp
+++ test.cpp
@@ -18,3 +18,31 @@ TEST_CASE("Floating point pow", "[pow]") {
         }
     }
 }
+
+TEST_CASE("Negative integral pow", "[pow]") {
+    for(double i = -1; i > -7; --i) {
+        for(int j = -1; i > -7; --i) {
+            REQUIRE( Approx(sm::pow(i,j)) == std::pow(i,j) );
+        }
+    }
+}
+
+TEST_CASE("Trignonometric", "[sin][cos]") {
+    for(double i = 0; i < 2*M_PI; i+=0.001) {
+        REQUIRE( Approx(sm::sin(i)).epsilon(0.0001) == std::sin(i) );
+        REQUIRE( Approx(sm::cos(i)).epsilon(0.005) == std::cos(i) );
+    }
+}
+
+TEST_CASE("e^x", "[exp]") {
+    for(double i = 0; i < 10; i+=0.3) {
+        REQUIRE( Approx(sm::exp(i)).epsilon((i+1)/1000) == std::exp(i) );
+    }
+}
+
+TEST_CASE("Natural logarithm", "[log]") {
+    for(double i = 0.3; i < 10; i+=0.3) {
+        REQUIRE( Approx(sm::log(i)).epsilon(0.05) == std::log(i) );
+    }
+}
+
